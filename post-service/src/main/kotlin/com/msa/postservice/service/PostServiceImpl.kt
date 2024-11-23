c2a1f9d0389aa58bef768e9b9be66fec89b4a765
heosangbeom <luv_3_u@naver.com>
2024-11-23 21:49:13 +0900

    feat(post): post 기능 서비스 추가
    - post 기능
      - service 계층 개발
      - service test code 작성

--- post-service/src/main/kotlin/com/msa/postservice/service/PostServiceImpl.kt
+++ post-service/src/main/kotlin/com/msa/postservice/service/PostServiceImpl.kt
@@ -6,6 +6,7 @@ import com.msa.postservice.repository.PostRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
+import java.util.stream.Collectors
 
 @Service
 @Transactional(readOnly = true)
@@ -20,9 +21,27 @@ class PostServiceImpl(
             title = postDto.title,
             content = postDto.content,
             userId = postDto.userId,
-            userame = postDto.username
+            username = postDto.username
         )
         postRepository.save(post)
         return postDto.also { it.postId = post.postId }
+    }
+
+    override fun getPostByPostId(postId: String): PostDto {
+        val findPost = postRepository.findByPostId(postId)
+        return PostDto(
+            postId = findPost.postId,
+            title = findPost.title,
+            content = findPost.content,
+            userId = findPost.userId,
+            username = findPost.username
+        )
+    }
+
+    override fun getPostsByUserId(userId: String): List<PostDto> {
+        val findPosts = postRepository.findByUserId(userId)
+        return findPosts.stream().map { p ->
+            PostDto(p.content, p.title, p.userId, p.username).toPostDto(p)
+        }.collect(Collectors.toList())
     }
 }
--- post-service/src/test/kotlin/com/msa/postservice/service/PostServiceImplTest.kt
+++ post-service/src/test/kotlin/com/msa/postservice/service/PostServiceImplTest.kt
@@ -4,16 +4,20 @@ import com.msa.postservice.dto.PostDto
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
 
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
+@Transactional
 class PostServiceImplTest(
     @Autowired private val postService: PostService
 ) {
 
+    @DisplayName("게시물 작성")
     @Test
     fun createPost() {
         val postDto = PostDto(
@@ -28,5 +32,47 @@ class PostServiceImplTest(
         assertThat(createPost.content).isEqualTo(postDto.content)
         assertThat(createPost.userId).isEqualTo(postDto.userId)
         assertThat(createPost.username).isEqualTo(postDto.username)
+    }
+
+    @DisplayName("게시물 단건 조회")
+    @Test
+    fun getPostByPostId(){
+        val postDto = PostDto(
+            content = "Hello world!",
+            title = "Hello world!",
+            userId = "user1",
+            username = "username1"
+        )
+        val createPost = postService.createPost(postDto)
+        val postId = createPost.postId
+        val findPost = postService.getPostByPostId(postId!!)
+        assertThat(findPost).isNotNull
+        assertThat(findPost.postId).isEqualTo(createPost.postId)
+        assertThat(findPost.title).isEqualTo(postDto.title)
+        assertThat(findPost.userId).isEqualTo(postDto.userId)
+        assertThat(findPost.username).isEqualTo(postDto.username)
+    }
+
+    @DisplayName("나의 게시물 리스트 조회")
+    @Test
+    fun getPostsByUserId(){
+
+        val postDto1 = PostDto(
+            content = "Hello world1!",
+            title = "Hello world1!",
+            userId = "user1",
+            username = "username1"
+        )
+        val postDto2 = PostDto(
+            content = "Hello world2!",
+            title = "Hello worl2!",
+            userId = "user1",
+            username = "username1"
+        )
+        postService.createPost(postDto1)
+        postService.createPost(postDto2)
+        val posts = postService.getPostsByUserId("user1")
+        assertThat(posts).hasSize(2)
+        assertThat(posts).extracting("title").contains(postDto1.title)
     }
 }
