00f6f1e3372965c4dc8be4fb087c31d382091691
heosangbeom <luv_3_u@naver.com>
2024-11-24 21:41:16 +0900

    feat(config): spring-cloud-bus 기능 추가
    - spring-cloud-bus
      - rabbitMQ 연동

--- post-service/src/main/kotlin/com/msa/postservice/controller/PostController.kt
+++ post-service/src/main/kotlin/com/msa/postservice/controller/PostController.kt
@@ -1,10 +1,21 @@
 package com.msa.postservice.controller
 
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class PostController {
+class PostController(
+    private val env: Environment
+) {
+
+    @GetMapping("/actuator/health_check")
+    fun healthCheck(): String {
+        return "{\"status\":\"UP\" \n" +
+                "\"token-key\":\"${env.getProperty("jwt.token.secrete")}\" \n" +
+                "\"token-time\":\"${env.getProperty("jwt.token.expiration_time")}\"}"
+
+    }
 
     @GetMapping("/posts")
     fun getPosts(): String {
--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 	implementation ("org.springframework.cloud:spring-cloud-starter-config")
 	implementation ("org.springframework.cloud:spring-cloud-starter-bootstrap")
 	implementation ("org.springframework.boot:spring-boot-starter-actuator")
+	implementation ("org.springframework.cloud:spring-cloud-starter-bus-amqp")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
